ea66ed67ab7536404be83405976a166e62c15073
Mikhail Orlov <crazyminer398@gmail.com>
2022-09-01 23:56:10 +0300

    created logic for new game

--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -14,12 +14,6 @@ def answers():
     return answer
 
 
-def cycle():
-    counter = (1, 2, 3)
-    for i in counter:
-        return
-
-
 def even_logic():
     number = randint(0, 100)
     print('Question: ' + str(number))
@@ -48,12 +42,13 @@ def calc_logic():
 def brain_even():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    counter = (1, 2, 3)
-    for i in counter:
+    i = 0
+    while i < 3:
         correct_answer = even_logic()
         answer = answers()
         if str(answer.lower()) == str(correct_answer.lower()):
             print('Correct!')
+            i += 1
         else:
             print(f'''"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}".
 Let's try again, {name}!''')
@@ -64,12 +59,13 @@ Let's try again, {name}!''')
 def brain_calc():
     name = welcome_user()
     print('What is the result of the expression?')
-    counter = (1, 2, 3)
-    for i in counter:
+    i = 0
+    while i < 3:
         correct_answer = calc_logic()
         answer = answers()
         if str(answer) == str(correct_answer):
             print('Correct!')
+            i += 1
         else:
             print(f''''{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
 Let's try again, {name}!''')
@@ -80,8 +76,8 @@ Let's try again, {name}!''')
 def brain_gcd():
     name = welcome_user()
     print('Find the greatest common divisor of given numbers.')
-    counter = (1, 2, 3)
-    for i in counter:
+    i = 0
+    while i < 3:
         first_number = randint(0, 100)
         second_number = randint(0, 100)
         print(f'Question: {first_number} {second_number}')
@@ -94,6 +90,7 @@ def brain_gcd():
         answer = answers()
         if str(correct_answer) == str(answer):
             print('Correct!')
+            i += 1
         else:
             print(f''''{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
 Let's try again, {name}!''')
@@ -104,8 +101,8 @@ Let's try again, {name}!''')
 def brain_progression():
     name = welcome_user()
     print('What number is missing in the progression?')
-    counter = (1, 2, 3)
-    for i in counter:
+    i = 0
+    while i < 3:
         item_position = randint(0, 9)
         first_number = randint(0, 20)
         regularity = randint(1, 15)
@@ -120,8 +117,42 @@ def brain_progression():
         arithmetic_progression = ' '.join(arithmetic_progression)
         print(f'Question: {arithmetic_progression}')
         answer = answers()
-        if str(answer) == (correct_answer):
+        if str(answer) == correct_answer:
             print('Correct!')
+            i += 1
+        else:
+            print(f''''{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
+Let's try again, {name}!''')
+            return
+    print(f'Congratulations, {name}!')
+
+
+def prime_number():
+    number = randint(0, 100)
+    i = 2
+    while number % i != 0:
+        i += 1
+    return i == number, number
+
+
+def brain_prime():
+    name = welcome_user()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    x = 0
+    while x < 3:
+        number = randint(0, 100)
+        print(f'Question: {number}')
+        i = 2
+        while number % i != 0:
+            i += 1
+        if i == number:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        answer = answers()
+        if str(correct_answer) == str(answer):
+            print('Correct!')
+            x += 1
         else:
             print(f''''{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.
 Let's try again, {name}!''')
