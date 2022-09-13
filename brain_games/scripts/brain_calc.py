367613d527238ba66c996ffdde8831d2e657d3e1
Mikhail Orlov <crazyminer398@gmail.com>
2022-09-13 17:57:14 +0300

    fixed bugs

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from brain_games.games.calc import save_question_and_correct_answer_of_calc, \
     RULES
-from brain_games.game_engine import start_the_game
+from brain_games.game_engine import run_the_game
 
 
 def main():
-    start_the_game(RULES, save_question_and_correct_answer_of_calc)
+    run_the_game(RULES, save_question_and_correct_answer_of_calc)
 
 
 if __name__ == '__main__':
