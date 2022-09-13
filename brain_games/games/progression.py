c4b12a7b998bd69376cd0afe859202c4bddfb244
Mikhail Orlov <crazyminer398@gmail.com>
2022-09-13 22:45:00 +0300

    fixed linter

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -27,7 +27,9 @@ def save_question_and_correct_answer_of_progression():
     initial_term = randint(TERM_LOWER_LIMIT, TERM_UPPER_LIMIT)
     common_difference = randint(DIFFERENCE_LOWER_LIMIT, DIFFERENCE_UPPER_LIMIT)
     end_of_progression = initial_term + common_difference * PROGRESSION_LENGTH
-    progression = create_arithmetic_sequence(initial_term, common_difference, end_of_progression)
+    progression = create_arithmetic_sequence(initial_term,
+                                             common_difference,
+                                             end_of_progression)
     correct_answer = progression[item_position]
     progression = create_string_progression(progression, item_position)
     question = progression
